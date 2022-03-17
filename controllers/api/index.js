d8cf9be696f3dfe6117cdd1704cc2e0c3e73d31f
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-16 21:01:33 -0500

    updated model, variable, and referencing names to coincide with Petrol Pirate nomenclature

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const priceRoutes = require('./priceRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/projects', priceRoutes);
 
 module.exports = router;
---
+++ controllers/api/priceRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Price } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPrice = await Price.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPrice);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const priceData = await Price.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!priceData) {
+            res.status(404).json({ message: 'No price found with this id!' });
+            return;
+        }
+
+        res.status(200).json(priceData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
