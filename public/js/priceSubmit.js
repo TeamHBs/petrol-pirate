ba0c6a648e38861034e71d648e90048baecb4cf4
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-18 00:02:53 -0500

    addional comments

--- public/js/priceSubmit.js
+++ public/js/priceSubmit.js
@@ -1,12 +1,14 @@
+const res = require('express/lib/response');
+
 const priceSubmit = async (event) => {
     event.preventDefault();
 
     const price = document.querySelector('#priceInput').value.trim();
     const name = document.querySelector('#stationInput').value.trim();
+    // const address =
+    // const zip =
 
-    console.log(name);
-
-    const response = await fetch('/api/stations', {
+    const newStation = await fetch('/api/stations', {
         method: 'POST',
         body: JSON.stringify({ name }),
         headers: {
@@ -14,12 +16,26 @@ const priceSubmit = async (event) => {
         },
     });
 
-    if (response.ok) {
+    if (newStation.ok) {
         //   get station id from db
+
+        console.log(`newStation=${newStation.body}`);
+        // const station_id = parsedStation.id;
+        const station_id = parsedStation.body.id;
+        // console.log(`parsedStation=${parsedStation}`);
+        console.log(`newStationID=${station_id}`);
+
         // use id to do post to price model
+
+        // const newPrice = await fetch('/api/prices/', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ price, station_id }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // });
+        // console.log(newPrice);
         // document.location.replace('/profile');
     } else {
-        alert('Failed to create project');
+        alert('Failed to create price');
     }
 
     // fetch('/api/users/login', {
@@ -29,12 +45,12 @@ const priceSubmit = async (event) => {
     //     res.json().then((data) => {
     //         console.log(data);
     //         const userId = data.id;
-    //         const response = fetch('/api/prices/', {
+    //         const newPrice = fetch('/api/prices/', {
     //             method: 'POST',
     //             body: JSON.stringify({ price, station, userId }),
     //             headers: { 'Content-Type': 'application/json' },
     //         });
-    //         console.log(response);
+    //         console.log(newPrice);
 
     // if (response.ok) {
     //     document.location.reload();
