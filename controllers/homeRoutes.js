2de7eb034f87a921930aaf1f8723007f48e40a27
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-17 19:47:14 -0500

    updated nav bar with submit page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
                 },
                 {
                     model: Station,
-                    attributes: ['name'],
+                    attributes: ['name', 'zip'],
                 },
             ],
         });
@@ -77,6 +77,18 @@ router.get('/', async (req, res) => {
 //         res.status(500).json(err);
 //     }
 // });
+
+router.get('/submit', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (!req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('submit', {
+        logged_in: req.session.logged_in,
+    });
+});
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
