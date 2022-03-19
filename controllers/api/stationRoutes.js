db576d2554e9a038737332d0ce50e4cabc363766
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-19 10:41:21 -0500

    fixed station post route, updated response to be usable as JSON

--- controllers/api/stationRoutes.js
+++ controllers/api/stationRoutes.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
     try {
         const stationData = await Station.create({ ...req.body });
 
-        res.status(200).json(stationData);
+        res.status(200).send(stationData.dataValues);
     } catch (err) {
         res.status(400).json(err);
     }
--- public/js/priceSubmit.js
+++ public/js/priceSubmit.js
@@ -1,42 +1,32 @@
-const res = require('express/lib/response');
-
 const priceSubmit = async (event) => {
     event.preventDefault();
 
     const price = document.querySelector('#priceInput').value.trim();
     const name = document.querySelector('#stationInput').value.trim();
-    // const address =
-    // const zip =
+    const address = document.querySelector('#addressInput').value.trim();
+    const zip = document.querySelector('#zipInput').value.trim();
 
     const newStation = await fetch('/api/stations', {
         method: 'POST',
-        body: JSON.stringify({ name }),
+        body: JSON.stringify({ name, address, zip }),
         headers: {
             'Content-Type': 'application/json',
         },
-    });
+    }).then((data) => data.json());
 
-    if (newStation.ok) {
-        //   get station id from db
-
-        console.log(`newStation=${newStation.body}`);
-        // const station_id = parsedStation.id;
-        const station_id = parsedStation.body.id;
-        // console.log(`parsedStation=${parsedStation}`);
-        console.log(`newStationID=${station_id}`);
-
-        // use id to do post to price model
-
-        // const newPrice = await fetch('/api/prices/', {
-        //     method: 'POST',
-        //     body: JSON.stringify({ price, station_id }),
-        //     headers: { 'Content-Type': 'application/json' },
-        // });
-        // console.log(newPrice);
-        // document.location.replace('/profile');
-    } else {
-        alert('Failed to create price');
-    }
+    // if (newStation.ok) {
+    //     //   get station id from db
+    //     // use id to do post to price model
+    //     const newPrice = await fetch('/api/prices/', {
+    //         method: 'POST',
+    //         body: JSON.stringify({ price, station_id }),
+    //         headers: { 'Content-Type': 'application/json' },
+    //     });
+    //     console.log(newPrice);
+    //     document.location.replace('/profile');
+    // } else {
+    //     alert('Failed to create price');
+    // }
 
     // fetch('/api/users/login', {
     //     method: 'GET',
