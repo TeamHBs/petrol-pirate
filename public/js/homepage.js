136b761d14a7b1030f63507d74f9e32cc6c06050
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-22 19:19:06 -0500

    added map to homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+// const pinArray = document.querySelectorAll('.pushpin').value;
+// const pinArray = [
+//     { lat: 41.8781, lon: -87.6298 },
+//     { lat: 42.0, lon: -86.0 },
+// ];
+
+// console.log(pinArray);
 
 const querySubmit = async (event) => {
     event.preventDefault();
@@ -19,6 +26,27 @@ const querySubmit = async (event) => {
 
     document.location.replace(`/?${filterChoice}=${filterInput}`);
 };
+
+function loadMapScenario() {
+    const lat = 41.8781;
+    const lon = -87.6298;
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        /* No need to set credentials if already passed in URL */
+        center: new Microsoft.Maps.Location(lat, lon),
+        mapTypeId: Microsoft.Maps.MapTypeId.road,
+        zoom: 10,
+    });
+
+    console.log(pinArray);
+
+    // loop through location array to create all the
+    pinArray.forEach((pin) => {
+        const loc = new Microsoft.Maps.Location(pin.lat, pin.lon);
+        const target = new Microsoft.Maps.Pushpin(loc);
+        //Add the pushpin to the map
+        map.entities.push(target);
+    });
+}
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
